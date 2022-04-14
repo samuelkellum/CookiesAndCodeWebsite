520db08d7783b2488b5af47c0a8b0c8adca99dab
Samuel Kellum <samuelkellum@gmail.com>
2022-04-14 12:37:25 -0500

    Static root

--- CookiesAndCodeWebsite/settings.py
+++ CookiesAndCodeWebsite/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
