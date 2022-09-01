b78ccfbe6244ca475e3855edf90c54caa49ae1ab
thegenghiskahn <stmbkahn2019@gmail.com>
2022-09-01 16:42:19 -0500

    minor change to update script

--- update_website_database.sh
+++ update_website_database.sh
@@ -24,6 +24,6 @@ python manage.py runscript google_test # update events
 # Overwrite remote heroku database with contents of local one
 # -----------------------------------------------------------
 pg_dump -Fc --no-acl --no-owner -h localhost -U bennett candctu > mydb1.dump # dump local database to compressed .dump file
-aws s3api put-object --bucket cookies-and-code-s3-bucket --key mydb1.dump --body mydb1.dump
+aws s3api put-object --bucket cookies-and-code-s3-bucket --key mydb1.dump --body mydb1.dump # hard-coded bucket name is okay as stated here: https://security.stackexchange.com/questions/214499/s3-bucket-name-obscurity-as-security
 SIGNED_URL=$(aws s3 presign s3://cookies-and-code-s3-bucket/mydb1.dump)
 heroku pg:backups:restore $SIGNED_URL DATABASE_URL --app cookies-and-code-site --confirm cookies-and-code-site
