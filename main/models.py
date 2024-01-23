cb04870f39423ca97a4d135fad94973bbc356926
Chris Lewis <61091285+cmlcode@users.noreply.github.com>
2024-01-22 21:57:33 -0600

    Update models.py
    Fix year quotations

--- main/models.py
+++ main/models.py
@@ -205,12 +205,6 @@ class SemesterMembershipStorage(models.Model):
 def get_curr_semester():
     # THIS MUST BE UPDATED TO THE CURRENT SEMESTER
     # Whatever semester this returns is the one for which points are calculated 
-    CURR_PART_OF_TERM = "Spring"
+    CURR_PART_OF_TERM = 'Spring'
     CURR_YEAR = '2024'
     return Semester.objects.get(part_of_term=CURR_PART_OF_TERM, year=CURR_YEAR)
-
-
-
-
-
-
